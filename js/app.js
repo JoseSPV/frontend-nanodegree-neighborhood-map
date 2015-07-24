b73108f5882eb5509bf89b4d79d5ef588b2e7d6e
Jose <jose.sancho.pagola@hotmail.com>
2015-07-24 15:49:02 +0200

    Bindings between markers and filter functione done :) git add js/app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /* HAY QUE AÑADIR COMPROBACIONES A LOS DATOS QUE SE PASAN A LAS FUNCIONES. SABER SI VIENEN VACÍOS Y TODO ESO NIGGER*/
-
+ //"use strict";
 
 var foursquareApi = {
   baseUrl: 'https://api.foursquare.com/v2/venues/search?client_id=TV0CTIVNSUNU01JHA1UT5ZJJUOMMZGOL5FDOPKFNYV5AS1X2&client_secret=B5D1VLCQLL1ACDJG50NXK3U0BHJSKD3Y4X3ZGRPCEAEB3JFA&v=20150715',
@@ -42,40 +42,85 @@ var InterestPoint = function(ipData) {
   self.address = ipData.address;
   self.coordinates = ipData.coordinates;
   self.category = ipData.category;
+  self.marker = ipData.marker;
+
+};
+
+var MapMarker = function (map, name, lat, lon, text) {
+
+  var self = this;
+
+  self.name = ko.observable(name);
+  self.lat  = ko.observable(lat);
+  self.lon  = ko.observable(lon);
+  self.text = ko.observable(text);
+  self.isVisible = ko.observable(true);
+
+  var marker = new google.maps.Marker({
+    position : new google.maps.LatLng(lat, lon),
+    animation: google.maps.Animation.DROP,
+    title: name
+  });
+
+  self.showHideMarker = ko.computed(function() {
+    if(self.isVisible()) {
+      marker.setMap(map);
+    } else {
+      marker.setMap(null);
+    }
+  });
 };
 
 // Viewmodel class that holds the current location and manages the location change
 var Search = function () {
   var self = this;
 
-  //Private variables
-  var pointData = {
-    name: '',
-    address: '',
-    coordinates: [],
-    category: ''
-  };
 
   // Stores the locations searched
   // TO-DO: Store in local cache and try to retrieve the information whenever the application is launched let it empty if no data is found
   self.locations = ko.observableArray([]);
   self.interestPoints = ko.observableArray([]);
-  // Keeps a pinter to the last location searched or the current one. NOTE: as we count from 0 we set the default value to -1 and it will be increase to 0 with the first element inserted
+  self.markers = ko.observableArray([]);
+
   self.address = ko.observable();
   self.coordinates = ko.observable(false);
   self.mapCreated = ko.observable(false);
+  self.currentMap = ko.observable();
+  self.searchTerm = ko.observable('');
 
+  // used to update the map
   self.mapData = ko.computed(function() {
         return {
             center: self.coordinates,
-            mapCreated: self.mapCreated
+            mapCreated: self.mapCreated,
+            map: self.currentMap
         };
   });
 
+ // used to update the ui
+ self.filterResults = ko.computed(function() {
+    // Represents a filtered list of planets
+    // i.e., only those matching the "typeToShow" condition
+    var query = self.searchTerm().toLowerCase();
+
+    return ko.utils.arrayFilter(self.interestPoints(), function(point) {
+      var isFiltered = point.name.toLowerCase().indexOf(query) >= 0;
+      point.marker.isVisible(isFiltered);
+
+      //console.log(self.interestPoints());
+
+      return isFiltered;
+    });
+
+  });
+
+
+
   self.populateInterestPoints = function (address) {
 
+
     // OJO hay que usar coordenadas el near no funciona bien con ciertas direcciones de autocomplete
-    var request = foursquareApi.setRequest('venue',address,50);
+    var request = foursquareApi.setRequest('venue',address,20);
 
     self.interestPoints.removeAll();
 
@@ -85,6 +130,22 @@ var Search = function () {
       success: function( data ) {
 
         var points = [];
+        var markers = [];
+        //Private variable to format the interest point data
+        var pointData = {
+          name: '',
+          address: '',
+          coordinates: [],
+          map: '',
+          category: ''
+        };
+
+        /*var markerData = {
+          lat: 0,
+          lng: 0,
+          map: {},
+          title: ''
+        }*/
 
         for(var i = 0; i < data.response.venues.length; i++) {
 
@@ -92,12 +153,19 @@ var Search = function () {
           pointData.name = data.response.venues[i].name;
           pointData.address = data.response.venues[i].location.address;
           pointData.coordinates = [data.response.venues[i].location.lat, data.response.venues[i].location.lng];
+          pointData.marker = new MapMarker(self.currentMap(),i.toString(),data.response.venues[i].location.lat,data.response.venues[i].location.lng,data.response.venues[i].name);
 
+          //separar y poner en otra función
+          /*markerData.lat = data.response.venues[i].location.lat;
+          markerData.lng = data.response.venues[i].location.lng;
+          markerData.title = data.response.venues[i].name;
+          markerData.map = self.currentMap();*/
+          //console.log(pointData);
           points.push(new InterestPoint(pointData));
+          //markers.push(new MapMarker(self.currentMap(),i.toString(),data.response.venues[i].location.lat,data.response.venues[i].location.lng,data.response.venues[i].name));
         }
 
         self.interestPoints(points);
-
       },
       error: function () {
         console.log('falló');
@@ -106,10 +174,10 @@ var Search = function () {
   };
 
   // We store the new location into our array
-  self.addLocation = function (newlocation) {
-    self.locations.push(newlocation);
-    self.address(newlocation.address);
-    self.coordinates(newlocation.coordinates);
+  self.addLocation = function (location) {
+    self.locations.push(location);
+    self.address(location.address);
+    self.coordinates(location.coordinates);
 
     self.populateInterestPoints(self.address());
   };
@@ -133,6 +201,7 @@ ko.bindingHandlers.updateMap = {
       self.map.setZoom(15);
 
       self.mapStatus().mapCreated(true);
+      self.mapStatus().map(self.map);
 
     } else if(self.mapStatus().mapCreated() && self.mapStatus().center() !== false) {
 
@@ -150,6 +219,7 @@ ko.bindingHandlers.getLocation = {
     init: function (element, valueAccessor, allBindings) {
 
         // Get the serchfield text to be used by the Autocomplete
+
         var self = this;
         self.newLocation = valueAccessor();
         self.searchField = element;
